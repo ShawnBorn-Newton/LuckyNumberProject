a6d4990e6155c6af43c6b875fb973583e57481fc
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-07 17:21:51 -0400

    done

--- LuckyNumberProject/Program.cs
+++ LuckyNumberProject/Program.cs
@@ -10,68 +10,75 @@ namespace LuckyNumberProject
     {
         static void Main(string[] args)
         {
-            int correct = 0;
-            Console.WriteLine("Let's play a game! Guess your lucky numbers.\nFirst enter a number.");
-            int firstNumber = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Now enter a bigger number.");
-            int secondNumber = int.Parse(Console.ReadLine());
-
-            while (firstNumber >= secondNumber)
+            string playAgain;
+            do
             {
-                Console.WriteLine("Please enter a valid number, larger then the first.");
-                secondNumber = int.Parse(Console.ReadLine());
-            }
+                Console.WriteLine("Let's play a game! Guess your lucky numbers.\nFirst enter a number.");
+                int firstNumber = int.Parse(Console.ReadLine());
 
+                Console.WriteLine("Now enter a bigger number.");
+                int secondNumber = int.Parse(Console.ReadLine());
 
-            Random luckyNumber = new Random();
-
-            int[] luckyArray = new int[6];
-            for (int i = 0; i < 6; i++)
-            {
-                luckyArray[i] = luckyNumber.Next(firstNumber, secondNumber);
-            }
-            int[] guessesArray = new int[6];
-            Console.WriteLine("We have six lucky number for you.\n" +
-                "For every number you get right you will recieve\n10 points for a jack pot of 60 points.\n"
-                + "What is you first guess?");
-
-
-            for (int j = 0; j < guessesArray.Length; j++)
-            {
-                guessesArray[j] = int.Parse(Console.ReadLine());
-                while (guessesArray[j] < firstNumber || guessesArray[j] > secondNumber) 
+                while (firstNumber >= secondNumber)
                 {
-                    Console.WriteLine("Please enter a valid number.");
-                    guessesArray[j] = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter a valid number, larger then the first.");
+                    secondNumber = int.Parse(Console.ReadLine());
                 }
-            }
-            
-              
-            
-            for (int k = 0; k < luckyArray.Length; k++)
+
+
+                Random luckyNumber = new Random();
+
+                int[] luckyArray = new int[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    luckyArray[i] = luckyNumber.Next(firstNumber, secondNumber);
+                }
+                int[] guessesArray = new int[6];
+                Console.WriteLine("We have six lucky number for you.\n" +
+                    "For every number you get right you will recieve\n10 points for a jack pot of 60 points.\n"
+                    + "Guess six numers between " + firstNumber + " and " + secondNumber + "."
+                    + "Press enter after each guess");
+
+
+                for (int j = 0; j < guessesArray.Length; j++)
+                {
+                    guessesArray[j] = int.Parse(Console.ReadLine());
+                    while (guessesArray[j] < firstNumber || guessesArray[j] > secondNumber)
+                    {
+                        Console.WriteLine("Please enter a valid number.");
+                        guessesArray[j] = int.Parse(Console.ReadLine());
+                    }
+                }
+
+
+                int correct = 0;
+                for (int k = 0; k < luckyArray.Length; k++)
                 {
                     Console.WriteLine("Lucky number: " + k);
 
-                foreach (int number in luckyArray)
-                {
+                    foreach (int number in luckyArray)
+                    {
 
-                    if (number == guessesArray[k])
-                    {
-                        correct += 1;
+                        if (number == guessesArray[k])
+                        {
+                            correct += 1;
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
-                    else
-                    {
-                        continue;
-                    }
-                }
                 }
                 int winings = correct * 10;
                 Console.WriteLine("you got: " + correct + " right!\n"
                     + "You won: " + winings + " points!");
 
+                Console.WriteLine("Do you want to play again? YES/NO");
+                playAgain = Console.ReadLine();
+            }
+            while (playAgain.ToUpper() == "YES");
 
-            
+
         }
     }
 }
